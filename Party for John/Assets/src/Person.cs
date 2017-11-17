7311aac4db1d6ee2fdc624c5b1744cd319d8bc6f
Matus Zdansky <matuszdansky@gmail.com>
2017-11-17 10:19:24 +0100

    Room.cs, Person.cs... basic prefabs

---
+++ Party for John/Assets/src/Person.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    [Tooltip("Person Name")]
+    public string Name;
+
+    [Tooltip("Plugged level - <0,1> = <Unplugged, Plugged>. The more the worse")]
+    public float PluggedLevel;
+
+    //[Tooltip("Relationship to Player - <0,1>. The more the better")]
+    //public bool TrustsMe;
+
+    [Tooltip("Peaple who have a boost towards me")]
+    public List<GameObject> LovedOnes;
+
+    public List<Action> Actions;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log(gameObject.name);
+    }
+}
---
+++ Party for John/Assets/src/Room.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    public bool IsLocked;
+
+    public bool IsPowered;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
