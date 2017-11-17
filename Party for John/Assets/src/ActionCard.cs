0b564081c09d9a438fcd3b5e15a8f73e6a7328e0
xsovao <acnero@gmail.com>
2017-11-17 17:44:56 +0100

    timery

--- Party for John/Assets/src/ActionCard.cs
+++ Party for John/Assets/src/ActionCard.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(SpriteRenderer))]
 public class ActionCard : MonoBehaviour
 {
     public Sprite BcgSelected;
@@ -12,37 +12,41 @@ public class ActionCard : MonoBehaviour
     [Tooltip("Cooldown in sec")]
     public float Cooldown;
 
+	private float Cooltime; 
     private bool IsSelected;
 
     // ------------------------------------------------------------------------------------------------------------------
     void Start()
     {
         IsSelected = false;
-
+		Cooltime = 0;
         RedrawSprite();
     }
 
     // ------------------------------------------------------------------------------------------------------------------
     private void OnMouseDown()
     {
-        GameObject gameState = GameObject.Find("GameState");
-        GameStateManager gsm = gameState.GetComponent<GameStateManager>();
+		if (Cooltime <= 0) {
+			GameObject gameState = GameObject.Find ("GameState");
+			GameStateManager gsm = gameState.GetComponent<GameStateManager> ();
 
-        gsm.SelectActionCard(this);
+			gsm.SelectActionCard (this);
+		}
     }
 
     // ------------------------------------------------------------------------------------------------------------------
     public void ApplyAction(Room r)
     {
         r.ImproveRoomState();
+		Cooltime = Cooldown;
     }
 
     // ------------------------------------------------------------------------------------------------------------------
     private void RedrawSprite()
     {
-        GetComponent<SpriteRenderer>().sprite = IsSelected
+        /*GetComponent<SpriteRenderer>().sprite = IsSelected
             ? BcgSelected
-            : BcgUnselected;
+            : BcgUnselected;*/
     }
 
     // ------------------------------------------------------------------------------------------------------------------
@@ -51,4 +55,14 @@ public class ActionCard : MonoBehaviour
         IsSelected = selected;
         RedrawSprite();
     }
+
+	public void FixedUpdate(){
+		if (Cooltime > 0)
+			Cooltime -= Time.deltaTime;
+		else
+			Cooltime = 0;
+
+		Image i = GetComponentsInChildren<Image> ()[1];
+			i.fillAmount = Cooltime / Cooldown;
+	}
 }
