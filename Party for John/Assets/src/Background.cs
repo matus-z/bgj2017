f8adec2c22ac58d7d5aaec6db950664a9c67debd
xsovao <acnero@gmail.com>
2017-11-23 00:19:54 +0100

    f

--- Party for John/Assets/src/Background.cs
+++ Party for John/Assets/src/Background.cs
@@ -6,8 +6,7 @@ public class Background : MonoBehaviour
 {
 	public Sprite day;
 	public Sprite night;
-<<<<<<< HEAD:Party for John/Assets/src/Background.cs
-	public Color c_day;
+public Color c_day;
 	public Color c_night;
 	public SpriteRenderer back;
 	// Use this for initialization
@@ -16,13 +15,7 @@ public class Background : MonoBehaviour
 	}
 
 	public void SetSprite(bool isDay){
-=======
-
-    // ------------------------------------------------------------------------------------------------------------------
-    public void SetSprite(bool isDay)
-    {
->>>>>>> 4d2cda09d777e80928ee3d6455719514f19f4273:Party for John/Assets/src/Background.cs
-		SpriteRenderer spr = GetComponent<SpriteRenderer> ();
+SpriteRenderer spr = GetComponent<SpriteRenderer> ();
 		if (spr != null) {
 			spr.sprite = isDay ? day : night;
 			back.color = isDay ? c_day : c_night;
